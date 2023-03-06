d97ca639fe005942d295b343346f40fa5411e991
Sahil-Navas <115899765+Sahil-Navas@users.noreply.github.com>
2023-03-06 12:32:37 -0500

    Added cpp file
    First version STILL IN PROGRESS

---
+++ TODOLISTUPDATED2.0.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+struct list 
+{
+    int priority;
+    std::string task;
+};    
+
+void listReader(int todoSize, std::vector<list> ls)
+{
+    for(int b = 0; b < todoSize; b++)
+    {
+        std::cout << "Task " << ls[b].priority << ": " << ls[b].task << std::endl;
+    }
+}
+
+
+int main()
+{
+    std::vector<list> ls;
+    int todoSize, priorityNo, inputNo; 
+    std::string taskTodo;
+
+    std::cout << "Type the number of tasks: ";
+    scanf("%d", &todoSize);
+
+    for(int j = 1; j <= todoSize; j++)
+    {
+        ls.push_back(list());
+    }
+
+    for(int i = 0; i < todoSize; i++)
+    {
+        std::cin >> priorityNo;
+        std::cin.ignore(256,'\n');
+        getline(std::cin, taskTodo);
+        ls[i].priority = priorityNo;
+        ls[i].task = taskTodo;
+    }
+
+    printf("1) Add a task\n");
+    printf("2) Read the list\n");
+    printf("3) Remove a task\n");
+    printf("4) Sort tasks alphabetically\n");
+    printf("5) Reset list\n");
+    printf("6) Exit program\n");
+
+    scanf("%d", &inputNo);
+
+    switch(inputNo)
+    {
+        case 1:
+
+            break;
+        case 2:
+            listReader(todoSize, ls);
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        default:
+        
+    }
+
+ }
